fbf7abf97e1fe15eb596ee77173a945d6c9f39ae
ducc <8889285+chrisspieler@users.noreply.github.com>
2026-03-06 14:44:44 -0700

    Don't load Steam DLL in QtAppSystem

--- engine/Sandbox.AppSystem/QtAppSystem.cs
+++ engine/Sandbox.AppSystem/QtAppSystem.cs
@@ -15,8 +15,7 @@ public class QtAppSystem
 	{
 		// get the current exe folder
 		string exeDir = AppContext.BaseDirectory;
-
-		LoadSteamDll();
+		
 		Api.Init();
 		NetCore.InitializeInterop( exeDir );
 		ErrorReporter.Initialize();
@@ -69,33 +68,10 @@ public class QtAppSystem
 		IToolsDll.Current.Exiting();
 
 		QApp.exit();
-
-		if ( steamApiDll != IntPtr.Zero )
-		{
-			NativeLibrary.Free( steamApiDll );
-			steamApiDll = default;
-		}
 	}
 
 	protected virtual void OnShutdown()
 	{
 
-	}
-
-	IntPtr steamApiDll = IntPtr.Zero;
-
-	/// <summary>
-	/// Explicitly load the Steam Api dll from our bin folder, so that it doesn't accidentally
-	/// load one from c:\system32\ or something. This is a problem when people have installed
-	/// pirate versions of Steam in the past and have the assembly hanging around still. By loading
-	/// it here we're saying use this version, and it won't try to load another one.
-	/// </summary>
-	protected void LoadSteamDll()
-	{
-		var dllName = $"{Environment.CurrentDirectory}\\bin\\win64\\steam_api64.dll";
-		if ( !NativeLibrary.TryLoad( dllName, out steamApiDll ) )
-		{
-			throw new System.Exception( "Couldn't load bin/win64/steam_api64.dll" );
-		}
 	}
 }
